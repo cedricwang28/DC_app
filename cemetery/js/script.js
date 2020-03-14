dd6db0c3670fd340e725c6455f27f2185b30ffca
Ajay Dubey <2beyajay@gmail.com>
2020-03-14 14:02:34 -0400

    added scroll buttons for mobile view

--- cemetery/js/script.js
+++ cemetery/js/script.js
@@ -3,32 +3,45 @@ window.onload = function () {
     window.addEventListener("scroll", scrollAnimate);
 
     let buttons = document.querySelectorAll(".quiz button");
-    
     buttons.forEach(button => {
         button.addEventListener("click", function () {
-            
+
             let soilId = button.dataset.soil;
-            
+
             let theQuiz = document.querySelector(`#${soilId} .quiz`);
             theQuiz.classList.add("hide")
 
             let theInfo = document.querySelector(`#${soilId} .information`);
-            
+
             theInfo.classList.remove("hide")
             // theInfo.classList.add("to-animate-in");
-            
         });
     });
 
+    let scrollUp = document.querySelector("#scroll-up");
+    let scrollDown = document.querySelector("#scroll-down");
+    let windowHeight = window.innerHeight;
+    console.log();
+    
+
+    scrollUp.addEventListener("mousedown", function () {
+        let windowPosition = window.scrollY;
+        window.scroll(0, windowPosition - windowHeight);
+    })
+
+    scrollDown.addEventListener("mousedown", function () {
+
+        let windowPosition = window.scrollY;
+        window.scroll(0, windowPosition + windowHeight);
+    })
 
 
 
-    document.querySelector(".close_icon").addEventListener("click",function(){
-        
+    // the close button
+    document.querySelector(".close_icon").addEventListener("click", function () {
+
         window.location.replace("../index.html");
     });
-
-
 };
 
 
@@ -37,13 +50,9 @@ function scrollAnimate() {
     let windowScrollPosition = window.scrollY;
     let toAnimateIn = document.querySelectorAll(".to-animate-in");
     console.log("animating");
-    
 
     toAnimateIn.forEach(element => {
-        
-        if (windowScrollPosition > (element.parentNode.offsetTop + (this.window.innerHeight / 1.5))) {  /* - this.window.innerHeight  */
-            console.log(element);
-            console.log(element.parentNode.offsetTop);
+        if (windowScrollPosition > (element.parentNode.offsetTop + (this.window.innerHeight / 1.5))) {
             element.classList.add("animated");
         }
     });
@@ -55,8 +64,14 @@ function scrollAnimate() {
 
 
 /* 
+
+remove the animation scroll event listener once all the sections have been animated
+
+
+// animate the drill rotation with this
+https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/rotate3d
+
+
 ask sully, 
     addition of the class happens to all elements at once, as soon as 1st element has the class.
-
-    (skeleton) left not working with position sticky
 */
