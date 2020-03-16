82d8f30e0ae8c4896f36f34876ea22f736346081
Emily <lam.emily813@gmail.com>
2020-03-16 04:55:12 -0400

    adds camera toggle switches, adds tree and fish assets

--- indigenousHistory/assets/js/main.js
+++ indigenousHistory/assets/js/main.js
@@ -10,10 +10,27 @@
 //         });
 //     }
 // });
-console.log("hello")
 
 
-document.querySelector(".close_icon").addEventListener("click",function(){
-        
-    window.location.replace("../index.html");
-});
+console.log("lakeview hello");
+
+let camera1Tag = document.querySelector("h1");
+camera1Tag.addEventListener("click", () => {
+    console.log("clicked camera 1 tag");
+    camera.setAttribute('camera', 'active', true);
+})
+let camera2Tag = document.querySelector("h2");
+camera2Tag.addEventListener("click", () => {
+    console.log("clicked camera 2 tag");
+    camera2.setAttribute('camera', 'active', true);
+})
+let camera3Tag = document.querySelector("h3");
+camera3Tag.addEventListener("click", () => {
+    console.log("clicked camera 3 tag");
+    camera3.setAttribute('camera', 'active', true);
+})
+let camera4Tag = document.querySelector("h4");
+camera4Tag.addEventListener("click", () => {
+    console.log("clicked camera 4 tag");
+    camera4.setAttribute('camera', 'active', true);
+})
