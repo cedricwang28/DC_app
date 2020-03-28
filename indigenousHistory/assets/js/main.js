743e1731f2dcc7fde1934b7503220234a80447c1
Emily <lam.emily813@gmail.com>
2020-03-28 19:10:30 -0400

    Updating files and images. Buttons for camera change styled.

--- indigenousHistory/assets/js/main.js
+++ indigenousHistory/assets/js/main.js
@@ -14,22 +14,22 @@
 
 console.log("lakeview hello");
 
-let camera1Tag = document.querySelector("h1");
+let camera1Tag = document.querySelector("#camera1-btn");
 camera1Tag.addEventListener("click", () => {
     console.log("clicked camera 1 tag");
     camera.setAttribute('camera', 'active', true);
 })
-let camera2Tag = document.querySelector("h2");
+let camera2Tag = document.querySelector("#camera2-btn");
 camera2Tag.addEventListener("click", () => {
     console.log("clicked camera 2 tag");
     camera2.setAttribute('camera', 'active', true);
 })
-let camera3Tag = document.querySelector("h3");
+let camera3Tag = document.querySelector("#camera3-btn");
 camera3Tag.addEventListener("click", () => {
     console.log("clicked camera 3 tag");
     camera3.setAttribute('camera', 'active', true);
 })
-let camera4Tag = document.querySelector("h4");
+let camera4Tag = document.querySelector("#camera4-btn");
 camera4Tag.addEventListener("click", () => {
     console.log("clicked camera 4 tag");
     camera4.setAttribute('camera', 'active', true);
